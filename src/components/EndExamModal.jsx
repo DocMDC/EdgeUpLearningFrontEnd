17f7b0a9477da71cb485a66c3a1ba2813accb7d3
DOCMDC <m.d.christensen3@gmail.com>
2023-12-31 23:57:01 -0600

    fixed minor z-index formatting error

--- src/components/EndExamModal.jsx
+++ src/components/EndExamModal.jsx
@@ -12,7 +12,7 @@ export default function EndExamModal({endExamModalState, setEndExamModalState, h
       <>
       {endExamModalState &&
         <>
-            <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-700 opacity-80 flex items-center justify-center z-50"></div>
+            <div className="absolute top-0 left-0 right-0 bottom-0 bg-gray-700 opacity-80 flex items-center justify-center z-[100]"></div>
             <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-80 w-80 bg-100 flex flex-col items-center rounded-md md:h-[375px] md:w-[375px] z-[100]">
                 <AiFillCloseCircle className="absolute top-2 right-2 text-xl cursor-pointer hover:text-600" onClick={() => setEndExamModalState(!endExamModalState)}/>
                 <h1 className="text-xl text-center border-b border-black mt-10 mb-10">End Block</h1>
